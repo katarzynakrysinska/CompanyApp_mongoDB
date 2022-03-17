44e7ae3ee00aa3c88fb7038524217a3a78477be9
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2022-03-18 00:52:51 +0100

    before task

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -13,7 +13,45 @@ describe('Department', () => {
 
     dep.validate(err => {
     expect(err.errors.name).to.exist;
+    });
   });
+
+  it('should throw an error if "name" is not a string', () => {
+
+    const cases = [{}, []];
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
+  it('should throw an error if "name" is too short or too long', () => {
+
+    const cases = ['ABC', 'abcd', 'Lorem ipsum, Lorem Ip']; // we test various cases, some of them are too short, some of them are too long
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
+  it('should not throw an error if "name" is okay', () => {
+
+    const cases = ['Management', 'Human Resources'];
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validate(err => {
+        expect(err).to.not.exist;
+      });
+  
+    }
+  
   });
 
 });
